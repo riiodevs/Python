94d2aa6a6375dc379bd3645ff2f55c3ca890c347
ReyoBian <136917663+riiodevs@users.noreply.github.com>
2023-07-30 21:45:46 +0700

    Create TipeData.py
    Tipe Data Python

---
+++ TipeData.py
@@ -0,0 +1,42 @@
+#tipe tipe data dalam python
+
+#Boolean ( true = 1 ) ( false = 0 )
+print(true)
+print(false)
+
+#String (huruf dll yang di apit tanda " " dan ' ' )
+print("Hello Word")
+print('Mari belajar python')
+
+
+#Integer ( Bilangan Bulan (Angka) )
+print(1)
+print(17)
+
+
+#Float ( bilangan ada sisa ( contoh : 1.3 ) )
+print(1.19)
+print(3.4)
+
+
+#Hexadecimal ( Menyatakan bilangan hexa " Bilang berbasis 16 " )
+print(1b)
+print(1c4)
+
+#Complex ( adalah bilangan yang menyatakan real dan imajiner )
+print(2+1j)
+
+#List ( tipe data yang menyimpan beberapa tipe data dan dapat dirubah )
+print ('xyz', 123, 1.23)
+
+#Tuple ( tipe data yang menyimpan tipe data dan tidak dapat berubah )
+print ( 'xyz', 123, 1.23 )
+
+
+#Dictionary ( meyimpan tipe data yang berpasangan )
+print {'nama' : reyo , 'id' : 1 }
+
+#contoh tipe data dictionary
+biodata = {'nama': reyo, 'id' : 1 }
+print(biodata) # untuk mencatak isi biodata
+print(type(biodata) # print jenis ini untuk melihat atu mencetak tipe data
