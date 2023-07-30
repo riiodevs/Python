41031418462a479762f650e8ed17d944b4a583d8
ReyoBian <136917663+riiodevs@users.noreply.github.com>
2023-07-30 22:06:49 +0700

    Create VariabelPython.py
    Variable adalah alokasi memori untuk menyimpan data yang dapat kita rubah tergantung isi variabel itu sendiri

---
+++ VariabelPython.py
@@ -0,0 +1,13 @@
+# Variable adalah alokasi memory untuk menyimpan data.
+
+# contoh variable
+namaDepan = 'Reyo'
+namaBelakang = 'Bian'
+userName = 'Reyobian'
+print('Biodata \n' namaDepan, "\n" namaBelakang, "\n" userName)
+
+# contoh variabel 
+panjang = 10
+lebar = 15
+luas = panjang * lebar
+print(luas)
